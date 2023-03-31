b228e91c47b72f41ea27bce33cb0973ca0afbe45
Akash S <108513566+akashnb0@users.noreply.github.com>
2023-03-31 13:48:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Alert Message</h1>
+     <button onClick={window['btn']}>Button</button>
+    </div>
+  );
+}
+
+export default App;
